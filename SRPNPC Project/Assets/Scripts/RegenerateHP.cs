80c86ad78c09887be32c25978e9acf1d3f621f2c
SopapillaKat <vellita2011@gmail.com>
2023-11-09 11:50:41 -0700

    trying

---
+++ SRPNPC Project/Assets/Scripts/RegenerateHP.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegenerateHP : MonoBehaviour
+{
+    private bool canHeal = false;
+
+    public StandardHealth standardHealth;
+    public event Action<float> OnHPPctChanged = delegate (float f) { };
+
+
+    public float CurrentHpPct
+    {
+        get { return (float)standardHealth.currentHealth / (float)standardHealth.startingHealth; }
+    }
+    public void HealHP()
+    {
+        if (canHeal)
+        {
+            //StartCoroutine(InvunlerabilityTimer());
+
+            standardHealth.currentHealth += 10;
+
+            OnHPPctChanged(CurrentHpPct);
+        }
+    }
+
+    public void Update()
+    {
+        Invoke("HealHP", 1.0f);
+    }
+    private IEnumerator InvunlerabilityTimer()
+    {
+        canHeal = false;
+        yield return new WaitForSeconds(1.0f);
+        canHeal = true;
+    }
+}
